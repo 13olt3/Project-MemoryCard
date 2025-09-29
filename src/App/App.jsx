e7bc6e17b5a118d5a7f0d6928daafadfd9282062
Thanh Nguyen <ehh_13olt3@hotmail.com>
2025-09-29 22:04:40 +1000

    First commit

---
+++ src/App/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import PictureCard from "./PictureCard/picture.jsx";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>project</div>
+      <PictureCard />
+    </>
+  );
+}
+
+export default App;
