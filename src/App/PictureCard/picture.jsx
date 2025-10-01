fa0c318ab9123911347311a1d1527b28240d9565
Thanh Nguyen <ehh_13olt3@hotmail.com>
2025-10-01 18:18:40 +1000

    Feat: implemented shuffle on click

--- src/App/PictureCard/picture.jsx
+++ src/App/PictureCard/picture.jsx
@@ -5,60 +5,56 @@ import { useState, useEffect } from "react";
 
 export default function PictureCard() {
   const [numberList, setNumberList] = useState(randomNumberList(15));
-  const [imgUrlArray, setImgUrlArray] = useState(null);
-  const [imgUrl, setImgUrl] = useState(null);
-  const [imgDataArray, setImgDataArray] = useState(null);
+  const [pokemonData, setPokemonData] = useState(null);
 
   useEffect(() => {
-    pokemonData().then((data) =>
-      setImgUrl(data.sprites.other.dream_world.front_default)
-    );
-  }, []);
-
-  useEffect(() => {
-    getAllPokemonData(numberList).then((data) => setImgUrlArray(data));
+    getAllPokemonData(numberList).then((data) => setPokemonData(data));
   }, [numberList]);
-
-  useEffect(() => {
-    if (imgUrlArray) {
-      let data = [];
-      for (let i = 0; i < imgUrlArray.length; i++) {
-        data.push(imgUrlArray[i].sprites.other.dream_world.front_default);
-      }
-      setImgDataArray(data);
-    }
-  }, [imgUrlArray]);
-
-  async function pokemonData() {
-    const data = await getPokemonInfo(15);
-    return data;
-  }
 
   async function getAllPokemonData(array) {
     let dataArray = [];
     for (let i = 0; i < array.length; i++) {
-      let thisData = await getPokemonInfo(array[i]);
+      let data = await getPokemonInfo(array[i]);
+      let thisData = {
+        key: data.name,
+        imgUrl: data.sprites.other.dream_world.front_default,
+      };
       dataArray.push(thisData);
     }
-
     return dataArray;
+  }
+
+  function jumbleArray() {
+    let currentIndex = numberList.length;
+    let array = numberList.slice(0);
+
+    while (currentIndex !== 0) {
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+    console.log(array);
+    setNumberList(array);
   }
 
   return (
     <div className="mainBox">
-      {imgDataArray ? (
-        imgDataArray.map((datapoint) => {
+      {pokemonData ? (
+        pokemonData.map((data) => {
           return (
-            <d>
-              <img style={{ width: "7em" }} src={datapoint}></img>
-            </d>
+            <div
+              key={data.key}
+              className="pictureCard"
+              onClick={() => jumbleArray()}
+            >
+              <img className="pokePicture" src={data.imgUrl}></img>
+            </div>
           );
         })
-      ) : (
-        <p>Loading Image...</p>
-      )}
-      {imgUrl ? (
-        <img style={{ width: "7em" }} src={imgUrl}></img>
       ) : (
         <p>Loading Image...</p>
       )}
